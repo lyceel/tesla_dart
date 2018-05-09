a5e7f84d8ee0ad9e4af103c217bd7539fbb456af
John McDole <jtmcdole@gmail.com>
2018-05-08 20:42:34 -0700

    Add some --help text
    ```shell
    tesla.dart
           --[no-]raw             dumps raw information
       -o, --[no-]show-options    decodes the VIN to print off options
       -c, --[no-]show-charge     current state of charge
                                  (defaults to on)
    
       -l, --[no-]show-climate    the current climate settings and measured temperatures
       -d, --[no-]show-drive      current location, heading, speed, gear, and power usage
       -g, --[no-]show-gui        user preferences (mph/kph, etc)
       -v, --[no-]show-vehicle    vehicle state (software verion, name, doors, etc)
           --[no-]show-all        shows all settings
       -h, --[no-]help            prints this help
       -a, --access-token
    ```

--- bin/tesla.dart
+++ bin/tesla.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
@@ -9,17 +10,41 @@ import 'package:tesla/vehicle.dart';
 
 Future main(List<String> args) async {
   var argParser = new ArgParser()
-    ..addFlag('raw', defaultsTo: false)
-    ..addFlag('show-options', defaultsTo: false)
-    ..addFlag('show-charge', defaultsTo: true)
-    ..addFlag('show-climate', defaultsTo: false)
-    ..addFlag('show-drive', defaultsTo: false)
-    ..addFlag('show-gui', defaultsTo: false)
-    ..addFlag('show-vehicle', defaultsTo: false)
-    ..addFlag('show-all', defaultsTo: false)
+    ..addFlag('raw', defaultsTo: false, help: 'dumps raw information')
+    ..addFlag('show-options',
+        abbr: 'o',
+        defaultsTo: false,
+        help: 'decodes the VIN to print off options')
+    ..addFlag('show-charge',
+        abbr: 'c', defaultsTo: true, help: 'current state of charge')
+    ..addFlag('show-climate',
+        abbr: 'l',
+        defaultsTo: false,
+        help: 'the current climate settings and measured temperatures')
+    ..addFlag('show-drive',
+        abbr: 'd',
+        defaultsTo: false,
+        help: 'current location, heading, speed, gear, and power usage')
+    ..addFlag('show-gui',
+        abbr: 'g', defaultsTo: false, help: 'user preferences (mph/kph, etc)')
+    ..addFlag('show-vehicle',
+        abbr: 'v',
+        defaultsTo: false,
+        help: 'vehicle state (software verion, name, doors, etc)')
+    ..addFlag('show-all', defaultsTo: false, help: 'shows all settings')
+    ..addFlag('help', abbr: 'h', help: 'prints this help')
     ..addOption('access-token', abbr: 'a');
 
   var results = argParser.parse(args);
+
+  if (results.wasParsed('help')) {
+    stderr.writeln("tesla.dart");
+    for (var line in LineSplitter.split(argParser.usage)) {
+      stderr.writeln("   $line");
+    }
+    await stderr.flush();
+    return;
+  }
   var raw = results['raw'];
   var showOptions = results['show-options'];
   var showCharge = results['show-charge'];
